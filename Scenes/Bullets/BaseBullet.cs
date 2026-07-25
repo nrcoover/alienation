b2669c61f08242c37259862d4c4b020716c4af41
nicklefritz <nrcoover@gmail.com>
2026-07-25 19:58:19 -0400

    Create inherited bullet scenes for player and enemies

---
+++ Scenes/Bullets/BaseBullet.cs
@@ -0,0 +1,42 @@
+using Godot;
+using System;
+
+public partial class BaseBullet : HitBox
+{
+	[Export] private VisibleOnScreenNotifier2D _visibleOnScreenNotifier2D;
+
+	private Vector2 _direction = Vector2.Up;
+	private float _speed = 100.0f;
+
+	public override void _Ready()
+	{
+		base._Ready();
+		SubscribeToSignals();
+	}
+
+	public override void _Process(double delta)
+	{
+		Position += _direction * _speed * (float)delta;
+	}
+
+	private void SubscribeToSignals()
+	{
+		_visibleOnScreenNotifier2D.ScreenExited += OnScreenExited;
+	}
+
+	public void BlowUp()
+	{
+		SetProcess(false);
+		QueueFree();
+	}
+
+	protected override void OnAreaEntered(Area2D area)
+	{
+		BlowUp();
+	}
+
+	private void OnScreenExited()
+	{
+		QueueFree();
+	}
+}
