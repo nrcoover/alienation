2a5c15faaae4c119c16bd186c277fd83e8ad0906
nicklefritz <nrcoover@gmail.com>
2026-07-25 10:23:52 -0400

    Revert "Fix spacing on player script"
    This reverts commit 3a36f462892ebe60ea0ba24f3cfc52fe6348c4a3.

--- Scenes/Player/Player.cs
+++ Scenes/Player/Player.cs
@@ -3,81 +3,82 @@ using System;
 
 public partial class Player : Area2D
 {
-	private const float MARGIN = 16.0f;
 
-	[Export] private float _speed = 250.0f;
-	[Export] private float _bulletSpeed = 250.0f;
-	[Export] private Vector2 _bulletDirection = Vector2.Up;
-	[Export] private int _healthBoost = 20;
+    private const float MARGIN = 16.0f;
 
-	[Export] private Sprite2D _sprite2D;
-	[Export] private AnimationPlayer _animationPlayer;
+    [Export] private float _speed = 250.0f;
+    [Export] private float _bulletSpeed = 250.0f;
+    [Export] private Vector2 _bulletDirection = Vector2.Up;
+    [Export] private int _healthBoost = 20;
 
-	private Vector2 _upperLeft;
-	private Vector2 _lowerRight;
+    [Export] private Sprite2D _sprite2D;
+    [Export] private AnimationPlayer _animationPlayer;
+
+    private Vector2 _upperLeft;
+    private Vector2 _lowerRight;
 
 	public override void _Ready()
 	{
-		AreaEntered += OnAreaEntered;
-		SetLimits();
+        AreaEntered += OnAreaEntered;
+        SetLimits();
 	}
 
-	public override void _Process(double delta)
+    public override void _Process(double delta)
 	{
-		UpdatePosition(delta);
+        UpdatePosition(delta);
 	}
 
-	private Vector2 GetInput()
-	{
-		Vector2 directionVector = new Vector2(
-			Input.GetAxis("left", "right"),
-			Input.GetAxis("up", "down")
-		);
+    private Vector2 GetInput()
+    {
+        Vector2 directionVector = new Vector2(
+            Input.GetAxis("left", "right"),
+            Input.GetAxis("up", "down")
+        );
 
-		HandleMovementAnimation(directionVector);
+        HandleMovementAnimation(directionVector);
 
-		GD.Print($"v.x: {directionVector.X}, v.y: {directionVector.Y}, v.Length(): {directionVector.Length()}, v.Normalized(): {directionVector.Normalized()}");
+        GD.Print($"v.x: {directionVector.X}, v.y: {directionVector.Y}, v.Length(): {directionVector.Length()}, v.Normalized(): {directionVector.Normalized()}");
 
-		return directionVector.Normalized();
-	}
+        return directionVector.Normalized();
+    }
 
-	private void SetLimits()
-	{
-		var viewport = GetViewportRect();
+    private void SetLimits()
+    {
+        var viewport = GetViewportRect();
 
-		_lowerRight = new Vector2(viewport.Size.X - MARGIN, viewport.Size.Y - MARGIN);
+        _lowerRight = new Vector2(viewport.Size.X - MARGIN, viewport.Size.Y - MARGIN);
 
-		_upperLeft = new Vector2(MARGIN, MARGIN);
-	}
+        _upperLeft = new Vector2(MARGIN, MARGIN);
+    }
 
-	private void HandleMovementAnimation(Vector2 direction) {
+    private void HandleMovementAnimation(Vector2 direction) {
 
-		var noMovement = 0;
+        var noMovement = 0;
 
-		if (direction.X == noMovement) {
-			_animationPlayer.Play("fly");
-			return;
-		}
+        if (direction.X == noMovement) {
+            _animationPlayer.Play("fly");
+            return;
+        }
 
-		if (direction.X > noMovement) {
-			_sprite2D.FlipH = true;
+        if (direction.X > noMovement) {
+            _sprite2D.FlipH = true;
 
-		} else if (direction.X < noMovement) {
-			_sprite2D.FlipH = false;
-		}
+        } else if (direction.X < noMovement) {
+            _sprite2D.FlipH = false;
+        }
 
-		_animationPlayer.Play("turn");
-	}
+        _animationPlayer.Play("turn");
+    }
 
-	private void UpdatePosition(double delta) {
-		var input = GetInput();
+    private void UpdatePosition(double delta) {
+        var input = GetInput();
 
-		Vector2 desiredPosition = GlobalPosition + input * (float)delta * _speed;
+        Vector2 desiredPosition = GlobalPosition + input * (float)delta * _speed;
 
-		GlobalPosition = desiredPosition.Clamp(_upperLeft, _lowerRight);
-	}
+        GlobalPosition = desiredPosition.Clamp(_upperLeft, _lowerRight);
+    }
 
-	private void OnAreaEntered(Area2D area)
-	{
-	}
+    private void OnAreaEntered(Area2D area)
+    {
+    }
 }
